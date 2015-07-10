b93caca49a93696bb191beb723f76a573c705c12
Justin Lovell <justin@justjuzzy.com>
2015-07-10 14:39:49 +0200

    Finalised demo for building node js apps

--- Node/index.js
+++ Node/index.js
@@ -6,7 +6,9 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  console.log('We are going to respond');
+  res.send('<h1>Hello world</h1>\n');
+  console.log('We have said hello :)');
 });
 
 app.listen(PORT);
